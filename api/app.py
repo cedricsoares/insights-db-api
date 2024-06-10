c512ffcb10ed61037061c803cfde7768f66f370c
Cedric Soares <cedricsoares@me.com>
2024-06-10 02:57:02 +0000

    Implement page data model

--- api/app.py
+++ api/app.py
@@ -1,11 +1,35 @@
-from flask_openapi3 import OpenAPI
+from datetime import datetime
+from flask_openapi3 import Info, OpenAPI, Tag
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+info = Info(title="insigts db api", version="1.0.0")
 
 app = OpenAPI(__name__)
 
+page_tag = Tag(name="page", descripion="a page")
 
-@app.route("/")
+
+class PagePath(BaseModel):
+    id: int = Field(..., description="Page id")
+
+
+class PageBody(BaseModel):
+    id: int = Field(..., description="Page id")
+    name: str = Field(..., description="Name of page")
+    created_at: Optional[datetime] = Field(..., description="Page creation date")
+
+
+class PageResponse(BaseModel):
+    code: int = Field(0, description="Status Code")
+    message: str = Field("ok", description="Exception Information")
+    data: Optional[PageBody]
+
+
+""" @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!" """
 
 
 if __name__ == "__main__":
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,2 @@
 def test_request_example(client):
-    res = client.get("/")
-    assert res.status_code == 200
+    pass
